81dee1855583c957e5d05d40730c0977d08febdd
Gabriel Sobral - BDS DataSolution <gsobral@bdsdatasolution.com.br>
2023-05-30 22:07:36 -0300

    feat: implementing To Do Project

---
+++ js/to-do-list.js
@@ -0,0 +1,142 @@
+function handleRenderToDoList(items) {
+  const listContainer = document.getElementById("to-do-list-container");
+
+  const allContent = items.reduce((accumulator, currentValue) =>
+    accumulator + 
+    `<li class="relative w-fit max-w-[500px] flex flex-col p-4 border-l-4 rounded-r-lg border-purple-600 bg-gray-900" id=${currentValue.id}>
+      <h3 class="text-sm font-medium text-gray-100">${currentValue.title}</h3>
+      <div class="mt-3 flex">
+        <button onclick="finishTodoButtonClick('${currentValue.id}')" type="button" class="bg-gray-900 shadow px-2 py-1.5 rounded text-sm font-medium text-gray-200 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-600">Finalizar</button>
+        <button onclick="updateTodoButtonClick('${currentValue.id}')" type="button" class="ml-1 bg-gray-900 shadow px-2 py-1.5 rounded text-sm font-medium text-gray-200 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-600">Editar</button>
+        <button onclick="removeTodoButtonClick('${currentValue.id}')" type="button" class="ml-1 bg-gray-900 shadow px-2 py-1.5 rounded text-sm font-medium text-gray-200 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-600">Remover</button>
+      </div>
+    </li>`
+  , "");
+
+  listContainer.innerHTML = allContent;
+}
+
+function handleRenderFinishedToDoList(items) {
+  const listContainer = document.getElementById("to-do-list-finished-container");
+
+  const allContent = items.reduce((accumulator, currentValue) =>
+    accumulator + 
+    `<li class="relative w-fit max-w-[500px] flex flex-col p-4 rounded-lg bg-green-900" id=${currentValue.id}>
+      <h3 class="text-sm font-medium text-gray-100">${currentValue.title}</h3>
+    </li>`
+  , "");
+
+  listContainer.innerHTML = allContent;
+}
+
+function clearInput() {
+  const input = document.querySelector("#text");
+  input.value = "";
+  input.focus();
+}
+
+
+function createToDo({ pendingItems, title }) {
+  const id = self.crypto.randomUUID();
+  const newItem = { id, title, status: "pending" }
+
+  return [ newItem, ...pendingItems ];
+}
+
+function finishToDo({ pendingItems, finishedItems, id }) {
+  const todoItem = pendingItems.find(item => item.id === id);
+
+  return {
+    newPendingItems: pendingItems.filter(item => item.id !== id),
+    newFinishedItems: [ { ...todoItem, status: "finished" }, ...finishedItems ]
+  }
+}
+
+function updateToDo({ pendingItems, id, title }) {
+  return pendingItems.map(item => {
+    if(item.id === id)
+      return { ...item, title };
+
+    return item;
+  })
+}
+
+function removeToDo({ pendingItems, id }) {
+  return pendingItems.filter(item => item.id !== id)
+}
+
+
+function getItems() {
+  const listPendingContainer = document.getElementById("to-do-list-container");
+  const listFinishedContainer = document.getElementById("to-do-list-finished-container");
+
+  function extractItems({ collection, status }) {
+    return Array.from(collection).map(item => {
+      const title = item.querySelector("h3").innerText;
+      
+      return { id: item.id, title, status }
+    })
+  }
+
+  const pendingItems = extractItems({ collection: listPendingContainer.children, status: "pending" });
+  const finishedItems = extractItems({ collection: listFinishedContainer.children, status: "finished" });
+  
+  return {
+    pendingItems,
+    finishedItems
+  }
+}
+
+
+function createTodoButtonClick() {
+  const inputValue = document.querySelector("#text").value;
+  const { pendingItems, finishedItems } = getItems();
+  const newToDoList = createToDo({ pendingItems: pendingItems, title: inputValue });
+  
+  handleRenderToDoList(newToDoList);
+  handleRenderFinishedToDoList(finishedItems);
+
+  clearInput()
+}
+
+function finishTodoButtonClick(id) {
+  const { pendingItems, finishedItems } = getItems();
+  const { newPendingItems, newFinishedItems } = finishToDo({ pendingItems, finishedItems, id });
+  
+  handleRenderToDoList(newPendingItems);
+  handleRenderFinishedToDoList(newFinishedItems);
+}
+
+function updateTodoButtonClick(id) {
+  const inputValue = document.querySelector("#text").value;
+
+  if(!inputValue)
+    return;
+
+  const { pendingItems, finishedItems } = getItems();
+  const newToDoList = updateToDo({ pendingItems: pendingItems, id, title: inputValue });
+  
+  handleRenderToDoList(newToDoList);
+  handleRenderFinishedToDoList(finishedItems);
+
+  clearInput();
+}
+
+function removeTodoButtonClick(id) {
+  const { pendingItems, finishedItems } = getItems();
+  const newToDoList = removeToDo({ pendingItems: pendingItems, id });
+  
+  handleRenderToDoList(newToDoList);
+  handleRenderFinishedToDoList(finishedItems);
+}
+
+function main() {
+  const { pendingItems } = getItems();
+
+  const addButton = document.querySelector("#add-todo-button");
+  addButton.addEventListener("click", createTodoButtonClick)
+
+  handleRenderToDoList(pendingItems);
+}
+
+main()
